2316807d306c062b1947831a1963b2edf01698ef
Islamttr <islamtutargs@gmail.com>
2023-04-26 22:45:30 +0300

    Change DriveSubsystem

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,13 +4,14 @@
 
 package frc.robot;
 
+import com.pathplanner.lib.server.PathPlannerServer;
+
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.AtarobGyro;
 
 
 /**
@@ -35,6 +36,8 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
+
+    PathPlannerServer.startServer(8240);
 
     m_robotContainer = new RobotContainer();
     //comp.disable();
